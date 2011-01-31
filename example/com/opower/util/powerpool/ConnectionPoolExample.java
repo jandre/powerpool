b5118296775124be6175949deb8b582ccc054aea
jennifer_andre <jennifer_andre@L-020241-A.SYMC.SYMANTEC.COM>
2011-01-31 01:17:59 -0500

    updating the README and example with a real example.

--- example/com/opower/util/powerpool/ConnectionPoolExample.java
+++ example/com/opower/util/powerpool/ConnectionPoolExample.java
@@ -1,12 +1,29 @@
 package com.opower.util.powerpool;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ConnectionPoolExample {
 
 	/**
 	 * @param args
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	 
+		SimpleConnectionPool pool = SimpleConnectionPool.createDefaultPool("org.hsqldb.jdbc.JDBCDriver",
+				"jdbc:hsqldb:mem:power-test", "sa", "");
+		
+		Connection connection = pool.getConnection();
+		
+		// do some stuff with the connection...
+		
+		pool.releaseConnection(connection);	// or, you can call connection.close();
+		
+		pool.stop();
+		
+		
 
 	}
 	
